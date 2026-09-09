8c19aeda3f611881a75cb8160c6e2707880d2b17
Anjali Dass <anjali95912@yahoo.com>
2026-09-09 14:49:49 +0530

    backend changes for authentication

---
+++ backend/app/generated/prisma/enums.d.ts
@@ -0,0 +1,7 @@
+export declare const Role: {
+    readonly ADMIN: 'ADMIN';
+    readonly RECRUITER: 'RECRUITER';
+    readonly CANDIDATE: 'CANDIDATE';
+};
+export type Role = (typeof Role)[keyof typeof Role];
+//# sourceMappingURL=enums.d.ts.map
